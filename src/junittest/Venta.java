4d8e106f3c6e6f7d7ed94a0c6c9c79328e4fb9d9
dyeudiel <dyeudiel>
2016-11-01 20:59:22 -0500

    laksd

--- src/junittest/Venta.java
+++ src/junittest/Venta.java
@@ -1,17 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package junittest;
 
-/**
- *
- * @author Administrator
- */
 public class Venta {
 	private float peso;
-	
 	
 	public void fijarPeso(float peso) throws Exception{
 		if(peso == 0){
@@ -20,4 +10,13 @@ public class Venta {
 		
 		this.peso = peso;
 	}
+        
+        private float precio;
+	
+	public float calcularprecio(float precio) throws Exception{
+            
+            return precio*peso;
+            
+        }        
+        
 }
