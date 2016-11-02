556575d2832a78d91903e21548131f183154b4e3
Said <smoya@ekoomedia.co>
2016-11-01 21:15:10 -0500

    =merge

--- src/junittest/Venta.java
+++ src/junittest/Venta.java
@@ -11,29 +11,15 @@ public class Venta {
 		
 		this.peso = peso;
 	}
-<<<<<<< HEAD
-    
 	
 	public float calcularprecio(float precio) throws Exception{
-            
-		return precio*peso;
-            
+		return precio*peso;    
     }        
         
-=======
-        
-        private float precio;
-	
-	public float calcularprecio(float precio) throws Exception{
-            
-            return precio*peso;
-            
-        }        
-        
-        private float existencias;
+    private float existencias;
 	
 	public float compararExistencias(float existecias) throws Exception{
-           return existecias;
-        }
->>>>>>> c91ce0b72aa4e2ca54ddca027ae5bcc6f52842a0
+		return existecias;
+    }
+
 }
